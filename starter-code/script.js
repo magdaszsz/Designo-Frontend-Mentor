a75f0e3306c7cfa3495b326f730f5278e4eaee65
Magda Sz <pesahson@gmail.com>
2021-08-16 17:47:42 +0200

    mobile menu/about page done

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,6 +1,14 @@
 const navToggler = document.querySelector('.nav-toggler');
-const nav = document.querySelector('nav')
+const nav = document.querySelector('nav');
+const list = document.querySelector('.nav-list');
 
 navToggler.addEventListener('click', () => {
-  nav.classList.toggle('show')
+  navToggler.classList.toggle('show');
+  if(list.classList.contains('slidein')){
+    list.classList.remove('slidein')
+    list.classList.add('slideout')
+  } else {
+    list.classList.remove('slideout');
+    list.classList.add('slidein')
+  }
 })
