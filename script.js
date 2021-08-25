9d2e72759a6b4b6a713bb16ea3e9f56435dbef3d
Magda Sz <pesahson@gmail.com>
2021-08-25 17:15:20 +0200

    add form validation

--- script.js
+++ script.js
@@ -1,14 +1,74 @@
-const navToggler = document.querySelector('.nav-toggler');
-const nav = document.querySelector('nav');
-const list = document.querySelector('.nav-list');
+const navToggler = document.querySelector(".nav-toggler");
+const nav = document.querySelector("nav");
+const list = document.querySelector(".nav-list");
 
-navToggler.addEventListener('click', () => {
-  navToggler.classList.toggle('show');
-  if(list.classList.contains('slidein')){
-    list.classList.remove('slidein')
-    list.classList.add('slideout')
+const form = document.querySelector("form");
+const userName = document.getElementById("name");
+const text = document.getElementById("textarea");
+const email = document.getElementById("email");
+const allInputs = document.querySelectorAll("input");
+
+navToggler.addEventListener("click", () => {
+  navToggler.classList.toggle("show");
+  if (list.classList.contains("slidein")) {
+    list.classList.remove("slidein");
+    list.classList.add("slideout");
   } else {
-    list.classList.remove('slideout');
-    list.classList.add('slidein')
+    list.classList.remove("slideout");
+    list.classList.add("slidein");
   }
-})
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+ function isEmail(email) {
+   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+     email
+   );
+ }
+
+ function setError(input, message) {
+   const inputWrapper = input.parentElement;
+   const small = inputWrapper.querySelector("small");
+   inputWrapper.className = "input-wrapper error";
+   small.innerText = message;
+ }
+
+ function setSuccess(input) {
+   const inputWrapper = input.parentElement;
+   inputWrapper.className = "input-wrapper success";
+ }
+
+function checkInputs() {
+  const userNameValue = userName.value.trim();
+  const textValue = text.value.trim();
+  const emailValue = email.value.trim();
+
+  if (userNameValue === "") {
+    setError(userName, "Field cannot be empty");
+  } else {
+    setSuccess(userName);
+  }
+
+  if (textValue === "") {
+    setError(text, "Field cannot be empty");
+  } else {
+    setSuccess(text);
+  }
+
+  
+  
+
+  if (emailValue === "") {
+    setError(email, "Field cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "Please enter a valid email");
+  } else {
+    setSuccess(email);
+  }
+
+  
+}
